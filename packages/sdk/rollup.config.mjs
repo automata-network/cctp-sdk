a5eaeda1bdfa7e05d7e837523b17a4f10ed2928f
duxiaofeng-github <csdxf297951292@gmail.com>
2023-12-04 18:58:23 +0800

    sdk is done

--- packages/sdk/rollup.config.mjs
+++ packages/sdk/rollup.config.mjs
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
+import json from "@rollup/plugin-json";
 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -12,16 +13,16 @@ const config = [
         file: "dist/index.mjs",
         format: "es",
         sourcemap: true,
-        external: ["lodash", "ethers"],
       },
       {
         file: "dist/index.cjs",
         format: "cjs",
         sourcemap: true,
-        external: ["lodash", "ethers"],
       },
     ],
+    external: ["lodash/merge", "ethers", "ethers/lib/utils"],
     plugins: [
+      json(),
       typescript(),
       NODE_ENV === "production" ? terser() : undefined,
     ].filter((item) => item != null),
---
+++ packages/sdk/src/utils.ts
@@ -0,0 +1,157 @@
+import { Contract, Signer, providers } from "ethers";
+import ERC20Abi from "./abis/ERC20.json";
+import tokenMessengerAbi from "./abis/TokenMessenger.json";
+import messageTransmitterAbi from "./abis/MessageTransmitter.json";
+import { Interface } from "ethers/lib/utils";
+
+export function getUSDCContract(usdcContractAddress: string, signer: Signer) {
+  return new Contract(usdcContractAddress, ERC20Abi, signer);
+}
+
+export function getTokenMessengerContract(
+  messengerContractAddress: string,
+  signer: Signer
+) {
+  return new Contract(messengerContractAddress, tokenMessengerAbi, signer);
+}
+
+export function getMessageTransmitterContract(
+  messageTransmitterContractAddress: string,
+  signer: Signer
+) {
+  return new Contract(
+    messageTransmitterContractAddress,
+    messageTransmitterAbi,
+    signer
+  );
+}
+
+export async function waitFor<T = void>(options: {
+  callback: (
+    next: () => void,
+    resolve: (data: T) => void,
+    reject: (e: unknown) => void
+  ) => Promise<void>;
+}) {
+  const { callback } = options;
+  let errorRetryCount = 0;
+  let getStatusCount = 0;
+  const backoffStrategyFactor = 1.5;
+  const throttleInterval = 3 * 1000;
+
+  function setTimer(
+    resolve: (data: T) => void,
+    reject: (err: unknown) => void
+  ) {
+    setTimeout(async () => {
+      try {
+        await callback(
+          () => {
+            errorRetryCount = 0;
+            getStatusCount += 1;
+            setTimer(resolve, reject);
+          },
+          resolve,
+          reject
+        );
+      } catch (e) {
+        if (errorRetryCount >= 3) {
+          reject(e);
+        } else {
+          getStatusCount += 1;
+          errorRetryCount += 1;
+          setTimer(resolve, reject);
+        }
+      }
+    }, throttleInterval * getStatusCount * backoffStrategyFactor);
+  }
+
+  return new Promise<T>((resolve, reject) => {
+    setTimer(resolve, reject);
+  });
+}
+
+export async function waitForTransaction(options: {
+  rpc: string;
+  txHash: string;
+}) {
+  await waitFor({
+    callback: async (next, resolve, reject) => {
+      const result = await getEthTransactionStatus(options);
+
+      if (result === TransactionStatus.Pending) {
+        next();
+      } else if (result === TransactionStatus.Failed) {
+        reject(new Error("transaction failed"));
+      } else {
+        resolve();
+      }
+    },
+  });
+}
+
+export async function waitForAttestation(options: {
+  apiHost: string;
+  messageHash: string;
+}) {
+  const { apiHost, messageHash } = options;
+
+  return waitFor<string>({
+    callback: async (next, resolve, reject) => {
+      const response = await fetch(`${apiHost}/attestations/${messageHash}`);
+      const attestationResponse = await response.json();
+
+      if (
+        attestationResponse &&
+        attestationResponse.status === "complete" &&
+        attestationResponse.attestation
+      ) {
+        resolve(attestationResponse.attestation);
+      } else {
+        next();
+      }
+    },
+  });
+}
+
+enum TransactionStatus {
+  Pending,
+  Failed,
+  Successful,
+}
+
+async function getEthTransactionStatus(options: {
+  rpc: string;
+  txHash: string;
+}) {
+  const { rpc, txHash } = options;
+
+  const provider = new providers.StaticJsonRpcProvider(rpc);
+
+  const receipt = await provider.getTransactionReceipt(txHash);
+
+  return receipt && receipt.status != null
+    ? receipt.status === 0
+      ? TransactionStatus.Failed
+      : TransactionStatus.Successful
+    : TransactionStatus.Pending;
+}
+
+export function parseLog(
+  iface: Interface,
+  logs: Array<any>,
+  methodName: string
+) {
+  for (const receiptLog of logs) {
+    try {
+      let parsedLog = iface.parseLog(receiptLog);
+
+      if (parsedLog.name === methodName) {
+        return { log: receiptLog, parsedLog };
+      }
+    } catch (e) {
+      // the correct log might be the other logs in the list
+      continue;
+    }
+  }
+}
